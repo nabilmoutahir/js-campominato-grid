1f208f17aabadf5ca0b4ea582a7b16bbe9e65dde
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-25 19:48:44 +0100

    feat: gather elements

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// * recupero elementi
+
+// button
+const buttonStartElement = document.getElementById('play-btn')
+console.log (buttonStartElement)
+
+// container
+const gridGameElement = document.getElementById('game-grid')
+console.log(gridGameElement)
+
+// generateGrid ()
