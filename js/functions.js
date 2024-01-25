2331ab66c96c9f14d0ede824aae5fb272dbc1dd9
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-25 19:49:12 +0100

    feat: add function generate grid

---
+++ js/functions.js
@@ -0,0 +1,62 @@
+// funzione genera gliglia
+function generateGrid (container) {
+
+    // svuoto HTML del container
+    gridGameElement.innerHTML = '';
+
+    // loop per ogni cella
+    for (let i = 0; i < 100; i++) {
+       
+        // genero cella tramite invocazione funzione generateCell
+        cellElement = generateCell()
+
+        // inserisco celle nel container
+        gridGameElement.append
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function generateCell () {
+
+    
+
+
+
+
+}
