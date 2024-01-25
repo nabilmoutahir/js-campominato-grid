e333437f124fd8be5bdcaecf0954be9aa6eb00aa
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-25 20:03:11 +0100

    feat: cell generator function

--- js/functions.js
+++ js/functions.js
@@ -5,58 +5,31 @@ function generateGrid (container) {
     gridGameElement.innerHTML = '';
 
     // loop per ogni cella
-    for (let i = 0; i < 100; i++) {
+    for (let i = 1; i <= 100; i++) {
        
         // genero cella tramite invocazione funzione generateCell
-        cellElement = generateCell()
+        cellElement = generateCell(i);
 
         // inserisco celle nel container
-        gridGameElement.append
+        gridGameElement.append(cellElement);
     }
 }
 
+// funzione genera cella
+function generateCell (Text) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function generateCell () {
-
+    // creo elemento html
+    const cell = document.createElement('div');
+    // aggiungo classi
+    cell.classList.add('cell');
     
+    cell.innerText = Text;
 
+    cell.addEventListener ('click', function (){
+        // comando per cambiare colore 
+        this.classList.toggle('active')
+    });
 
-
-
+    // ritorno valore
+    return cell;
 }
